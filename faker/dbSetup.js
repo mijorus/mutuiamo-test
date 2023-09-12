a0d54b4e9f7faf44410f91606f45150d5c1eb80a
Lorenzo Paderi <39067225+mijorus@users.noreply.github.com>
2023-09-12 11:17:21 +0200

    wip

---
+++ faker/dbSetup.js
@@ -0,0 +1,10 @@
+import 'dotenv/config';
+import * as db from "../src/db.js";
+
+await db.testConnection();
+
+// const { Bank } = await import("../src/models/Bank.js");
+// const { Product } = await import("../src/models/Product.js");
+// const { Prospect } = await import("../src/models/Prospect.js");
+const { User } = await import("../src/models/User.js");
+await db.sqlz.sync({ });
